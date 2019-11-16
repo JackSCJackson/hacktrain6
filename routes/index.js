e45f73437dcd5efd281ca55f5f09bdba20fae9cd
Anirudh E <anirudh.ksg@gmail.com>
2019-11-16 00:30:14 +0000

    Chat endpoint for testing

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res ) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/about', function(req, res) {
+  res.send('test page');
+});
+
+
+router.post('/chat', function(req, res) {
+  res.send(req.body.message);
+});
+
 
 module.exports = router;
